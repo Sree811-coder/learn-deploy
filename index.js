e623d52ac23e5a288bb5300501cedec4a4160fa0
Sree811-coder <deepaasreevk2001@gmail.com>
2025-07-01 11:37:59 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hi Welcome to learning deployment');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
